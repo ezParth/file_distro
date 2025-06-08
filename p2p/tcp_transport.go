e9813da860207a7e50410fc25c0e6dce0f04b4ec
ezParth <parthsingh55m@gmail.com>
2025-06-08 15:12:06 +0530

    added tcp, tcp-transport and tcp-test

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,49 @@
+package p2p
+
+// public fuctions on top and private function on bottom
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+	mu            sync.RWMutex
+
+	peer map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP error: %s\n", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("new incoming connection: %+v", conn)
+
+}
---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Peer is a remote node
+type Peer interface {
+}
+
+// Handles comm b/w nodes (this can be of the form TCP, UDP, websockets...)
+type Transport interface {
+}
